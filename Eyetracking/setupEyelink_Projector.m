921ea69d2d37239c5e76c0b20a16bcf5fe486654
Alex Rezai <arezai98@vpnlmanager.stanford.edu>
2019-03-12 10:06:55 -0700

    Using screen from Toonotopy instead of opening a new one. Specifying location of calibration and validation dots.

--- Eyetracking/setupEyelink_Projector.m
+++ Eyetracking/setupEyelink_Projector.m
@@ -1,38 +1,22 @@
-function setupEyelink_Projector( edfName )
+function setupEyelink_Projector( edfName, win )
 % setupEyelink_Toon setups eyelink system for eye tracking with the 
 % projector.
 %
 %   setupEyelink_Projector( edfName )
 %       edfName - (string) what you would like to call your edf (should be
 %                          no more than 8 characters)
+%       win - PsychToolBox window pointer
 %
 % AR March 2019
 
-% Set scaling factor for shrinking screen resolution
+% Set scaling factor for shrinking screen resolution to fit on projector
 shrinkWidth = .2; % This fraction of the width will be taken off
 shrinkHeight = .2; % This fraction of the height will be taken off
 
 % Initialize eyelink and check to make sure Eyelink is online
 if EyelinkInit() ~= 1
-    error(['Cannot connect to eye tracker'])
+    error(['Cannot connect to eye tracker. Try turning off wifi.'])
 end
-
-%% Setup screen
-
-% Make sure all psychtoolbox screens are closed
-Screen('CloseAll');
-
-% Skip warning message on screen
-Screen('Preference', 'SkipSyncTests', 1);
-% Get screen number
-screenNumber = max(Screen('Screens'));
-% Open window
-win = Screen('OpenWindow',screenNumber);
-Screen('FillRect',win,127);
-% Set this window to highest priority
-Priority(MaxPriority(win));
-% Flip screen
-Screen('Flip',win);
 
 %% Eyelink setup
 
@@ -44,10 +28,11 @@ edf = Eyelink('Openfile',edfName);
 % Check to make sure file was created
 if edf ~= 0
     Eyelink('Shutdown')
-    error(['Could not create EDF file ' edfName]);
+    error(['Could not create EDF file ' edfName ...
+           '. Try turning off wifi']);
 end
 
-% Setting eyelink preferences
+% Setting eyelink preferences and parameters
 Eyelink('command', 'add_file_preamble_text ''dynamic stim eyetracking''');
 Eyelink('command','screen_distance = 940'); % Distance from eye tracker 
                                            % camera to mirror image of
@@ -82,34 +67,30 @@ Eyelink('command', ...
 % controls how far the dots are from the bottom of the screen (if height-1, 
 % dots could appear at very bottom).
 rect = [width*shrinkWidth, height*shrinkHeight, ...
-        width - width*shrinkWidth, height-1];
-%{
-Eyelink('command','screen_pixel_coords = %ld %ld %ld %ld', rect(1), rect(2), ...
-        rect(3), rect(4)); % sets physical.ini to screen pixels
-Eyelink('message', 'DISPLAY_COORDS %ld %ld %ld %ld', rect(1), rect(2), ...
-        rect(3), rect(4));    
-%}
+        width - width*shrinkWidth, height-200];
+
 % Set screen coordinates
 Eyelink('command','screen_pixel_coords = %ld %ld %ld %ld', 0, 0, ...
         width-1, height-1); % sets physical.ini to screen pixels
 Eyelink('message', 'DISPLAY_COORDS %ld %ld %ld %ld', 0, 0, width-1, ...
         height-1);
+    
 % Override default calibration and validation targets
 Eyelink('command','generate_default_targets = NO');
 Eyelink('command','calibration_samples = 3');
 Eyelink('command','calibration_targets = %d,%d %d,%d %d,%d',...
-        rect(1),rect(4), rect(3),rect(4) width*.5,rect(2));
+        rect(1),rect(4), rect(3),rect(4), width*.5,rect(2));
 Eyelink('command','validation_samples = 3');
 Eyelink('command','validation_targets = %d,%d %d,%d %d,%d',...
-        rect(1),rect(4), rect(3),rect(4) width*.5,rect(2));
+        rect(1),rect(4), rect(3),rect(4), width*.5,rect(2));
 
 %% Calibrate
-fprintf('Run calibration, validation, and drift correction now. When done, hit "Output/Record"');
+fprintf('\n\nRun calibration and validation now. When done, hit "Output/Record"\n\n\n');
 
-% Run calibration, validation and drift correction
+% Run calibration and validation
 EyelinkDoTrackerSetup(el);
 
-% Close Screen
-Screen('CloseAll');
+% Run drift correction
+EyelinkDoDriftCorrect(el,width*.5, height*.5); % Specified fixation point
 
 end
